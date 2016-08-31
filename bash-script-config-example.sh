2865dd350fbf19040d8f74c554bb181dbe5bda4a
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-08-31 23:04:56 +0600

    Add new config

--- bash-script-config-example.sh
+++ bash-script-config-example.sh
@@ -4,16 +4,27 @@
 
 . simple.conf
 
-setting_general
-echo $_source
+settings_general
+_SRC=$_source
+echo $_SRC
 
-setting_additional
+settings_additional
 echo $_source
 
 echo $_param1
+echo $_remote
 
 if $_param1; then
   echo TRRUUE
 else
   echo FALLSEE
 fi
+
+if [[ -f simple2.conf ]]; then
+  echo Exist simle2
+  . simple2.conf
+  settings2_general
+  _SRC=$_source
+fi
+
+echo $_SRC
